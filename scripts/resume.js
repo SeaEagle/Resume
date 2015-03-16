d528e8947940428fcff83f7b06cba04649706bc6
SeaEagle <admin@Eagle.local>
2015-03-16 16:27:23 +0800

    初始化相关的网页文件

---
+++ scripts/resume.js
@@ -0,0 +1,41 @@
+/**
+ * resume 本应用特定脚本
+ */
+$(document).ready(function(){
+
+    //window对象
+    var $window = $(window);
+
+    //
+    $('section[data-type="story"]').each(function(){
+
+        //当前section
+        var $this = $(this), $thisOffset = $this.offset();
+
+        //根据浏览器高度设置section的高度
+        //$this.height($window.height()+200);
+        var size = 'auto'+' '+($window.height())+'px';
+        $this.css({
+            backgroundSize: size
+        });
+
+        //缓存data数据
+        $this.data('speed', $this.attr('data-speed'));
+
+        //当浏览器滚动时
+        $(window).scroll(function(){
+            //section在视图内
+            if( ( $window.scrollTop() + $window.height() ) >= ( $thisOffset.top ) &&
+                ( $thisOffset.top + $this.height() ) >= ( $window.scrollTop()) ){
+
+                $('html, body').animate({
+                    scrollTop:$this.next().offset().top
+                }, 1000, function() {
+                });
+
+            }//section在视图内
+        });//window scroll
+
+    });
+
+});
