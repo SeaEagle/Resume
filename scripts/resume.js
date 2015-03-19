5d108dc49e894b3388ba13336c802c2571796a71
SeaEagle <admin@Eagle.local>
2015-03-19 15:23:53 +0800

    修复滚动翻页问题（未做平滑处理）

--- scripts/resume.js
+++ scripts/resume.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
         //获取当前选择的导航项
         var index = $('.menu .item').index(this);
         //更新页面显示
-        updatePageDisplay(index);
+        updatePageDisplay(currentIndex, index);
     });
 
     //页面鼠标滚动时
@@ -33,28 +33,58 @@ $(document).ready(function(){
         }
     });
 
+    //鼠标向上翻页
     function pageUp(){
         if( currentIndex > 0 ){
-            currentIndex--;
+            updatePageDisplay(currentIndex, currentIndex-1);
         }
-        updatePageDisplay(currentIndex);
+
     }
 
+    //鼠标向下翻页
     function pageDown(){
         if( currentIndex < ( pagesCount - 1 ) ){
-            currentIndex++;
+            updatePageDisplay(currentIndex, currentIndex+1);
         }
-        updatePageDisplay(currentIndex);
     }
 
     //根据索引, 更新页面显示
-    function updatePageDisplay(index){
-        //更新页码索引
-        updateCurrentIndex(index);
-        //激活导航当前选项, 将其他选项去除激活
-        activeChooseItem(index);
+    function updatePageDisplay(fromIndex, toIndex){
         //滚动到指定的页
-        scrollChooseItem(index);
+        scrollChooseItem(fromIndex, toIndex);
+        //激活导航当前选项, 将其他选项去除激活
+        activeChooseItem(toIndex);
+        //更新页码索引
+        updateCurrentIndex(toIndex);
+    }
+
+    //滚动到指定的页
+    function scrollChooseItem(fromIndex, toIndex){
+
+        //console.info($('section[data-type="story"]').eq(fromIndex).attr('class'));
+        //console.info($('section[data-type="story"]').eq(toIndex).attr('class'));
+
+        $('html, body').animate({
+            scrollTop:$('.container section').eq(toIndex).offset().top
+        }, {
+            duration:1000,
+            complete:function() {
+                //滚动完之后的操作, 可在此增加某些页的动画
+                scrolling = false;
+            }
+        });
+
+        //$('section[data-type="story"]').eq(fromIndex).animate({
+        //    width: '0%'
+        //}, 1000, 'easeInOutCubic', function(){
+        //    $('section[data-type="story"]').eq(toIndex).show();
+        //});
+
+    }
+
+    //激活index对应的导航项, 去除其他选项的激活状态
+    function activeChooseItem(index){
+        $('.menu .item').eq(index).addClass('active').siblings().removeClass('active');
     }
 
     //更新当前页索引
@@ -63,23 +93,4 @@ $(document).ready(function(){
         currentIndex = index;
     }
 
-    //激活index对应的导航项, 去除其他选项的激活状态
-    function activeChooseItem(index){
-        $('.menu .item').eq(index).addClass('active').siblings().removeClass('active');
-    }
-
-    //滚动到指定的页
-    function scrollChooseItem(index){
-        $('html, body').animate({
-            scrollTop:$('.container section').eq(index).offset().top
-        }, {
-            duration:2000,
-            easing: 'easeIn',
-            complete:function() {
-                //滚动完之后的操作, 可在此增加某些页的动画
-                scrolling = false;
-            }
-        });
-
-    }
 });
